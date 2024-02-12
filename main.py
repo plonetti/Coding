512f5e4cb15ed2082aa6c4b04dcef61fa2930a67
plonetti <lonetti@paciunzio.istruzioneer.it>
2024-02-12 13:19:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,5 @@
 """
-MODULO 1:
-- due cose su python
-- python.org download e setup
-- documentazione di riferimento https://www.w3schools.com/python/default.asp
-- ide : visual code
-
-MODULO 2:
-- variabili, tipi di dati, operatori , commenti
-- input e output dei dati
-- matematica...numeri e operatori ..modulo math
-- stringhe
-
-MODULO 3:
-- if ..elif... else
-- and / or
-
-MODULO 4:
-- while
-- for
-
-MODULO 5:
-- list, tuple,set, dict
-- 
-
-MODULO 6:
-- le funzioni
+Corso base Python
 
 """
 
